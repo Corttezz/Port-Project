b2c5599e7e3eca22df89eceab59d592af3bdf8e9
Daniel Cortez <106662629+Corttezz@users.noreply.github.com>
2023-01-25 18:26:29 -0300

    att
    att

--- assets/js/main.js
+++ assets/js/main.js
@@ -35,38 +35,53 @@ const skillsContent = document.getElementsByClassName('skills__content'),
     skillsHeader = document.querySelectorAll('.skills__header');
 
 
-    function toggleSkills(){
-        let itemClass = this.parentNode.className
+function toggleSkills() {
+    let itemClass = this.parentNode.className
 
-        for(i = 0; i < skillsContent.length; i++){
-            skillsContent[i].className = 'skills__content skills__close'
-        }
-        if(itemClass === 'skills__content skills__close'){
-            this.parentNode.className = 'skills__content skills__open'
-        }
+    for (i = 0; i < skillsContent.length; i++) {
+        skillsContent[i].className = 'skills__content skills__close'
     }
+    if (itemClass === 'skills__content skills__close') {
+        this.parentNode.className = 'skills__content skills__open'
+    }
+}
 
-    skillsHeader.forEach((el) =>{
-        el.addEventListener('click', toggleSkills)
-    })
+skillsHeader.forEach((el) => {
+    el.addEventListener('click', toggleSkills)
+})
 
-    /* =============== QUALIFICATION TABS =============== */
+/* =============== QUALIFICATION TABS =============== */
 
-    const tabs = document.querySelectorAll('[data-target]'),
-          tabsContents = document.querySelectorAll('[data-content]')
+const tabs = document.querySelectorAll('[data-target]'),
+    tabsContents = document.querySelectorAll('[data-content]')
 
-    tabs.forEach(tab => {
-        tab.addEventListener('click', () => {
-            const target = document.querySelector(tab.dataset.target)
+tabs.forEach(tab => {
+    tab.addEventListener('click', () => {
+        const target = document.querySelector(tab.dataset.target)
 
-            tabsContents.forEach(tabContent =>{
-                tabContent.classList.remove('qualification__active')
-            })
-            target.classList.add('qualification__active')
-
-            tabs.forEach(tab => {
-                tab.classList.remove('qualification__active')
-            })
-            tab.classList.add('qualification__active')
+        tabsContents.forEach(tabContent => {
+            tabContent.classList.remove('qualification__active')
         })
-    })      
+        target.classList.add('qualification__active')
+
+        tabs.forEach(tab => {
+            tab.classList.remove('qualification__active')
+        })
+        tab.classList.add('qualification__active')
+    })
+})
+
+/* =============== SERVICES MODAL =============== */
+const modalViews = document.querySelectorAll('.services__modal'),
+    modalBtns = document.querySelectorAll('.services__button'),
+    modalCloses = document.querySelectorAll('.services__modal-close')
+
+let modal = function (modalClick) {
+    modalViews[modalClick].classList.add('active-modal')
+}
+
+modalBtns.forEach((modalBtn, i) => {
+    modalBtn.addEventListener('click', () => {
+        modal(i)
+    })
+})
